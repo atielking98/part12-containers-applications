a6d0e70953a3762903fe1501de19d36edde05319
Allison Tielking <atielkin@alumni.stanford.edu>
2023-03-08 18:54:02 -0500

    finish through 12.12

--- todo-app/todo-backend/routes/index.js
+++ todo-app/todo-backend/routes/index.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const redis = require('../redis')
 
 const configs = require('../util/config')
 
 let visits = 0
 
 /* GET index data. */
-router.get('/', async (req, res) => {
+router.get('/', async (_, res) => {
   visits++
 
   res.send({
     ...configs,
     visits
   });
+});
+
+router.get('/statistics', async (_, res) => {
+  res.send({ added_todos: await redis.getAsync('added_todos') || 0 });
 });
 
 module.exports = router;
